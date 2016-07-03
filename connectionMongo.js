41e6a8624e37eab1cadaed9bba2c33746fa54eb1
Alex Berrocal <alex.berrocal@paygroup.com>
2016-07-03 20:06:08 -0300

    Add Ejemplo1

--- connectionMongo.js
+++
@@ -1,9 +0,0 @@
-/**
-author: Alex Berrocal T.
-date: 2015/01/12
-description: Connection for MongoDB.
-**/
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/bot')
-
-module.exports = mongoose
--- usuarios.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('./connectionMongo')
-
-var Schema = mongoose.Schema
-
-var userSchema = new Schema({
-  '_id':String,
-})
-
-module.exports = mongoose.model('user', userSchema, 'users')
