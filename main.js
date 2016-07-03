1eb439e8823e52bf93e4dcb5d6372e095b8c77d7
Alex Berrocal <alex.berrocal@paygroup.com>
2016-07-03 20:10:07 -0300

    Add Ejemplo2

--- main.js
+++ main.js
@@ -15,3 +15,12 @@ bot.onText(/\/textosimple$/, (msg, match)=>{
   let text = match[0]
   bot.sendMessage(fromId, 'Como has digitado:' + text + ', te devuelvo un texto simple')
 })
+
+bot.onText(/\/textomenu$/, (msg, match)=>{
+  const fromId = msg.from.id
+  let text = match[0]
+  let keyboard = [['Opcion 1 \uD83C\uDF63','Opcion 2 \uD83C\uDF75'],['Opcion 3 \uD83C\uDF63','Opcion 4 \uD83C\uDF75']]
+  let reply_markup = {'keyboard':keyboard, 'resize_keyboard':true, 'one_time_keyboard': true, 'selective': true}
+  let options = {'reply_markup':reply_markup}
+  bot.sendMessage(fromId,'Como has digitado:' + text + ', te devuelvo un menu de opciones',options)
+})
